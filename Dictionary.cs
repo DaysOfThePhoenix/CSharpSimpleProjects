602d7cdddb9093612efae4468fa7cba665ec5f0c
Zlatil Veselinov Zlatilov <zlatilveselinovzlatilov@gmail.com>
2013-02-02 14:20:43 +0200

    Updated file

--- Dictionary.cs
+++ Dictionary.cs
@@ -1,8 +1,4 @@
-﻿//Solution for:
-//A dictionary is stored as a sequence of text lines containing words and their explanations. 
-//Write a program that enters a word and translates it by using the dictionary.
-
-using System;
+﻿using System;
 using System.Text;
 
 class Dictionary
