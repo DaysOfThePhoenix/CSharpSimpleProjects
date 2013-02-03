bc24ea203aafeda2b5b3602c669433b0359d6547
DaysOfThePhoenix <zlatilveselinovzlatilov@gmail.com>
2013-02-03 19:25:48 +0200

    Added commentaries

--- TitleAndBody.cs
+++ TitleAndBody.cs
@@ -1,4 +1,8 @@
-﻿using System;
+//Solution for:
+//Write a program that extracts from given HTML file its title (if available),
+//and its body text without the HTML tags.
+
+using System;
 using System.Text;
 using System.Text.RegularExpressions;
 
@@ -10,7 +14,8 @@ class TitleAndBody
   <head><title>News</title></head>
   <body><p><a href=""http://academy.telerik.com"">Telerik Academy</a> aims to provide free real-world practical training for young 
 people who want to turn into skillful .NET software engineers.</p></body></html>";
-        foreach (object titleTaged in Regex.Matches(html, @"<title>(.*?)</title>"))
+
+        foreach (object titleTaged in Regex.Matches(html, @"<title>(.*?)</title>")) //extractiong the tittle
         {
             string title = titleTaged.ToString();
             int indexTitleStart = title.IndexOf(">")+1;
@@ -27,11 +32,12 @@ people who want to turn into skillful .NET software engineers.</p></body></html>
         int indexBodyEnd = html.IndexOf("</body>", indexBodyStart);
         StringBuilder bodySB = new StringBuilder();
 
-        for (int i = indexBodyStart; i < indexBodyEnd; i++)
+        for (int i = indexBodyStart; i < indexBodyEnd; i++) //extracting the body part of the file
         {
             bodySB.Append(html[i]);
         }
-        foreach (object tag in Regex.Matches(bodySB.ToString(),@"<(.*?)>"))
+        
+        foreach (object tag in Regex.Matches(bodySB.ToString(),@"<(.*?)>")) //removing tags
         {
             int index = bodySB.ToString().IndexOf(tag.ToString());
             bodySB.Remove(index,tag.ToString().Length);
